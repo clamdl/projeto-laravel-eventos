b1ee3226bc3c040e3dfbd848ee3bb3bb57c7c1f5
Clarice de Medeiros Devêza Lacerda <clamdl@outlook.com>
2021-02-16 12:08:30 -0300

    :arrow_up: Cria controller, model, migrations e integração com o banco de dados

--- hdcevents/resources/views/products.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.main')
-
-@section('title', 'Produtos')
-@section('content')
-   
-    <h1> Página de Produtos!</h1>
-
-@endsection
--- hdcevents/resources/views/welcome.blade.php
+++ hdcevents/resources/views/welcome.blade.php
@@ -6,4 +6,8 @@
     <h1> Hello,world!</h1>
     <img src="/img/banner.jpg" alt="banner">
 
+    @foreach($events as $event)
+        <p>{{ $event->title }} -- {{ $event->description }}</p>
+    @endforeach
+
 @endsection
