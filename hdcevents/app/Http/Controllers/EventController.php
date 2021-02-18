20dadc388ba564c0e9f18ae9fca5675ca99584ad
Clarice de Medeiros Devêza Lacerda <clamdl@outlook.com>
2021-02-18 11:05:37 -0300

    :sparkles: Adiciona recurso que permite o cliente adicionar imagens do evento

--- hdcevents/app/Http/Controllers/EventController.php
+++ hdcevents/app/Http/Controllers/EventController.php
@@ -28,6 +28,20 @@ class EventController extends Controller
         $event->private = $request->private;
         $event->description = $request->description;
 
+        //Image Upload
+        if($request->hasFile('image') && $request->file('image')->isValid()){
+
+            $requestImage = $request->image;
+
+            $extension = $requestImage->extension();
+
+            $imageName = md5($requestImage->getClientOriginalName() . strtotime("now")) . "." . $extension;
+
+            $request->image->move(public_path('img/events'), $imageName);
+
+            $event->image = $imageName;
+        }
+
         $event->save();
 
         //Event::create($request->all());
